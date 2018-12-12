5f4b6737f471f15d81a0168e8913bd2e724d1032
songgx <guanxin@7net.cc>
2018-12-12 15:37:38 +0800

    重写 onMeasure() 来修改已有的 View 的尺寸； 已有的尺寸是super.onMeasure()测量保存的结果，测量完成会调用setMeasuredDimension()

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -7,6 +7,16 @@ import android.widget.ImageView;
 
 /**
  * 需要把它写成正方形的 ImageView
+ * 重写 onMeasure() 来修改已有的 View 的尺寸；
+ * 第一类自定义的具体做法
+ * 也就是重写 onMeasure() 来修改已有的 View 的尺寸的具体做法：
+ *
+ * 重写 onMeasure() 方法，并在里面调用 super.onMeasure()，触发原有的自我测量；
+ * 在 super.onMeasure() 的下面用 getMeasuredWidth() 和 getMeasuredHeight()
+ * 来获取到之前的测量结果，并使用自己的算法，根据测量结果计算出新的结果；
+ * 调用 setMeasuredDimension() 来保存新的结果。
+ *
+ * @author sgx
  */
 public class Practice01SquareImageView extends ImageView {
     public Practice01SquareImageView(Context context) {
@@ -33,5 +43,10 @@ public class Practice01SquareImageView extends ImageView {
         // 然后通过计算，让宽度和高度一致
 
         // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
+
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+        int width = Math.min(measuredWidth,measuredHeight);
+        setMeasuredDimension(width,width);
     }
 }
